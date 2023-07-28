26c3a62e13faad89a1db5796a3563222a3d36e07
serbulent <serbulentars@gmail.com>
2023-07-28 13:34:09 +0300

    28.07.2023

--- src/test/java/pages/AllovercommercePage.java
+++ src/test/java/pages/AllovercommercePage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
-git
+
 public class AllovercommercePage {
 
 
