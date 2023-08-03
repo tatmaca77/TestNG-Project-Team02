06fc7e2032b8c7c977de6370d6201d4d7bf1871a
Ekrem ASLAN <aslnekrem96@gmail.com>
2023-08-03 07:27:00 +0200

    ekremAslan

--- src/test/java/tests/US_0015/TC_01.java
+++ src/test/java/tests/US_0015/TC_01.java
@@ -1,50 +1,61 @@
 package tests.US_0015;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
-import pages.Allovercommerce;
+import pages.AllovercommercePage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ExtentReport;
 import utilities.ReusableMethods;
 
-public class TC_01 extends ReusableMethods {
+
+import static utilities.ReusableMethods.*;
+
+public class TC_01 extends ExtentReport {
     @Test
     public void test01() {
+        extentTest = extentReports.createTest("US0015_TC01","Test Raporu");
         Driver.getDriver().get(ConfigReader.getProperty("alloverUrl"));
-        Allovercommerce allovercommerce = new Allovercommerce();
-        allovercommerce.signIn1.click();
-        allovercommerce.email.sendKeys(ConfigReader.getProperty("email"));
-        allovercommerce.password.sendKeys(ConfigReader.getProperty("password"));
-        allovercommerce.signIn2.click();
+        extentTest.info("Anasayfaya gidildi");
+        AllovercommercePage allovercommercePage = new AllovercommercePage();
+        allovercommercePage.signIn1EA.click();
+        allovercommercePage.emailEA.sendKeys(ConfigReader.getProperty("emailEA"));
+        allovercommercePage.passwordEA.sendKeys(ConfigReader.getProperty("passwordEA"));
+        extentTest.info("Satici olarak Log in olundu");
+        allovercommercePage.signIn2EA.click();
         bekle(5);
         Driver.getDriver().findElement(By.tagName("body")).sendKeys(Keys.END);
-        allovercommerce.myAccount.click();
-        allovercommerce.storeManager.click();
-        allovercommerce.product.click();
-        allovercommerce.addNew.click();
-        scroll(allovercommerce.scroll7Things);
+        allovercommercePage.myAccountEA.click();
+        extentTest.info("My Account sayfasina gidildi");
+        allovercommercePage.storeManagerEA.click();
+        extentTest.info("Store Manager a sayfasina gidildi");
+        allovercommercePage.productEA.click();
+        extentTest.info("Product sayfasina gidildi");
+        allovercommercePage.addNewEA.click();
+        extentTest.info("Add New secenegine tiklandi");
+        scroll(allovercommercePage.scroll7ThingsEA);
         bekle(3);
         Driver.getDriver().findElement(By.xpath(ConfigReader.getProperty("inventory"))).click();
-        scroll(allovercommerce.scroll7Things);
+        scroll(allovercommercePage.scroll7ThingsEA);
         bekle(3);
         Driver.getDriver().findElement(By.xpath(ConfigReader.getProperty("shipping"))).click();
-        scroll(allovercommerce.scroll7Things);
+        scroll(allovercommercePage.scroll7ThingsEA);
         bekle(3);
         Driver.getDriver().findElement(By.xpath(ConfigReader.getProperty("attributes"))).click();
-        scroll(allovercommerce.scroll7Things);
+        scroll(allovercommercePage.scroll7ThingsEA);
         bekle(3);
         Driver.getDriver().findElement(By.xpath(ConfigReader.getProperty("linked"))).click();
-        scroll(allovercommerce.scroll7Things);
+        scroll(allovercommercePage.scroll7ThingsEA);
         bekle(3);
         Driver.getDriver().findElement(By.xpath(ConfigReader.getProperty("seo"))).click();
-        scroll(allovercommerce.scroll7Things);
+        scroll(allovercommercePage.scroll7ThingsEA);
         bekle(3);
         Driver.getDriver().findElement(By.xpath(ConfigReader.getProperty("advanced"))).click();
-        scroll(allovercommerce.scroll7Things);
+        scroll(allovercommercePage.scroll7ThingsEA);
         bekle(3);
         Driver.getDriver().findElement(By.xpath(ConfigReader.getProperty("inventory"))).click();
         bekle(5);
+        extentTest.pass("Inventory, Shipping, Attributes, Linked, Seo, Advanced menülerinin tiklanabilir ve görülebilir oldugunu dogrulandi");
         tumSayfaResmi("TC_01");
 
 
--- src/test/java/tests/US_0015/TC_02.java
+++ src/test/java/tests/US_0015/TC_02.java
@@ -3,38 +3,58 @@ package tests.US_0015;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
-import pages.Allovercommerce;
+import pages.AllovercommercePage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ExtentReport;
 import utilities.ReusableMethods;
 
-public class TC_02 extends ReusableMethods {
+import static utilities.ReusableMethods.*;
+
+public class TC_02 extends ExtentReport {
     @Test
     public void test01() {
+        extentTest = extentReports.createTest("US0015_TC02","Test Raporu");
         Driver.getDriver().get(ConfigReader.getProperty("alloverUrl"));
-        Allovercommerce allovercommerce = new Allovercommerce();
-        allovercommerce.signIn1.click();
-        allovercommerce.email.sendKeys(ConfigReader.getProperty("email"));
-        allovercommerce.password.sendKeys(ConfigReader.getProperty("password"));
-        allovercommerce.signIn2.click();
+        extentTest.info("Anasayfaya gidildi");
+        AllovercommercePage allovercommercePage = new AllovercommercePage();
+        allovercommercePage.signIn1EA.click();
+        allovercommercePage.emailEA.sendKeys(ConfigReader.getProperty("emailEA"));
+        allovercommercePage.passwordEA.sendKeys(ConfigReader.getProperty("passwordEA"));
+        allovercommercePage.signIn2EA.click();
+        extentTest.info("Satici olarak Log in olundu");
         bekle(5);
         Driver.getDriver().findElement(By.tagName("body")).sendKeys(Keys.END);
-        allovercommerce.myAccount.click();
-        allovercommerce.storeManager.click();
-        allovercommerce.product.click();
-        allovercommerce.addNew.click();
-        scroll(allovercommerce.scroll7Things);
+        allovercommercePage.myAccountEA.click();
+        extentTest.info("My Account sayfasina gidildi");
+        allovercommercePage.storeManagerEA.click();
+        extentTest.info("Store Manager a sayfasina gidildi");
+        allovercommercePage.productEA.click();
+        extentTest.info("Product sayfasina gidildi");
+        allovercommercePage.addNewEA.click();
+        extentTest.info("Add New secenegine tiklandi");
+        scroll(allovercommercePage.scroll7ThingsEA);
         bekle(3);
         Driver.getDriver().findElement(By.xpath(ConfigReader.getProperty("inventorySKU"))).sendKeys("Aslan");
+        extentTest.info("SKU kutucuguna bir metin girildi");
         Driver.getDriver().findElement(By.xpath(ConfigReader.getProperty("inventoryManageStock"))).click();
+        extentTest.info("Manage Stock secenegini isaretlendi");
         Driver.getDriver().findElement(By.xpath(ConfigReader.getProperty("inventoryStockQty"))).sendKeys("12");
-        ddmIndex(allovercommerce.ddminventoryAllowBackorders,2);
+        extentTest.info("Stock Qty kutucuguna gecerli bir rakam girildi");
+        ddmIndex(allovercommercePage.ddminventoryAllowBackordersEA,2);
+        extentTest.info("Allow Backorders' dan Allow secenegi secildi");
         Driver.getDriver().findElement(By.xpath(ConfigReader.getProperty("inventorySoldIndividually"))).click();
+        extentTest.info("Sold Individually secenegi tiklandi");
         bekle(3);
+        extentTest.info("Islemlerin yapilabildigi dogrulandi");
         tumSayfaResmi("TC_02");
-        allovercommerce.submit.click();
+        allovercommercePage.submitEA.click();
+        extentTest.info("Submit secenegi tiklandi");
         bekle(2);
+        extentTest.pass("Zorunlu alanlarin doldurulmadigina dair uyari alindi");
         tumSayfaResmi("TC_02_Uyari");
+
+
 
 
 
